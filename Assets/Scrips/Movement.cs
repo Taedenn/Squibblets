b6cb2c02ec145059739322e666fc4ada6fbff287
Morrigan <clumsychickenthe3rd@gmail.com>
2023-01-30 11:30:20 -0700

    Revert "Created movement with arrow keys"
    This reverts commit b248329eb244a8f6e5c4f8c2e8bccd1b796b6c94.

--- Assets/Scrips/Movement.cs
+++ Assets/Scrips/Movement.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float movement_speed = 15;
+    [SerializeField] float movement_speed = 0.01f;
 
     void Update()
     {
-        // transform.Translate(0, movement_speed, 0);
-        float x_movement = Input.GetAxis("Horizontal") * movement_speed * Time.deltaTime;
-        float y_movement = Input.GetAxis("Vertical") * movement_speed * Time.deltaTime;
-        transform.Translate(x_movement, 0, 0);
-        transform.Translate(0, y_movement, 0);
+        transform.Translate(0, movement_speed, 0);
     }
 }
