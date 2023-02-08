75db49f9f4a1559398d6dc3aad78a7eda4bb08f7
Nathan Hutton <109482458+Nathan-Hutton@users.noreply.github.com>
2023-02-07 18:28:36 -0700

    Added encounters and some UI

--- Assets/Scripts/Encounter.cs
+++ Assets/Scripts/Encounter.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Encounter : MonoBehaviour
 {
     bool in_encounter = false;
     GameObject player;
+    Button correct_button;
     void OnTriggerEnter2D(Collider2D other) {
+        GameObject canvas = transform.GetChild(0).gameObject;
+        canvas.SetActive(true);
+        correct_button = canvas.transform.GetChild(1).gameObject.GetComponent<Button>();
+        correct_button.onClick.AddListener(Deletion);
+
         if(other.CompareTag("Player"))
         {
             player = other.gameObject;
@@ -15,16 +22,10 @@ public class Encounter : MonoBehaviour
         }   
     }
 
-    void Update() {
-        if (in_encounter)
-        {
-            if(Input.GetKey(KeyCode.Space))
-            {
-                player.GetComponent<Movement>().enabled = true;
-                Destroy(gameObject);
-
-            }
-        }    
+    void Deletion() 
+    {
+        player.GetComponent<Movement>().enabled = true;
+        Destroy(gameObject);
     }
 
 }
