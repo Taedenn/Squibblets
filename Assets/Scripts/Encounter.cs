b6b5a7925c65ef06d682998543f2c1e91c8b38dc
Nathan Hutton <109482458+Nathan-Hutton@users.noreply.github.com>
2023-02-12 14:08:15 -0700

    Now only 1 canvas is used. Made a fix where you cannot run into the same enemy twice

--- Assets/Scripts/Encounter.cs
+++ Assets/Scripts/Encounter.cs
@@ -2,22 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Encounter : MonoBehaviour
 {
+    [SerializeField] string question;
+    [SerializeField] int correct_answer;
     GameObject player;
     Canvas canvas;
-    Button correct_button;
+    GameObject correct_button;
+    GameObject incorrect_button;
+    TextMeshProUGUI correct_text;
+    TextMeshProUGUI incorrect_text;
+    TextMeshProUGUI question_text;
     [SerializeField] float deletion_delay = 2f;
     [SerializeField] AudioClip winSFX;
     AudioSource audio_player;
     ParticleSystem particles;
     SpriteRenderer enemy_renderer;
+    bool isDead = false;
 
     void Start() {
-        canvas = transform.GetComponentInChildren<Canvas>();
-        canvas.enabled = false;
-        correct_button = canvas.transform.Find("Correct_button").gameObject.GetComponent<Button>();
+        canvas = FindFirstObjectByType<Canvas>();
+
+        question_text = canvas.transform.Find("Question_text").GetComponent<TextMeshProUGUI>();
+        question_text.enabled = false;
+
+        correct_button = canvas.transform.Find("Correct_button").gameObject;
+        incorrect_button = canvas.transform.Find("Incorrect_button").gameObject;
+        
+        correct_text = correct_button.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        incorrect_text = incorrect_button.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        correct_button.SetActive(false);
+        incorrect_button.SetActive(false);
 
         enemy_renderer = gameObject.GetComponent<SpriteRenderer>();
         particles = transform.GetComponentInChildren<ParticleSystem>();
@@ -25,14 +43,23 @@ public class Encounter : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
     }
     void OnTriggerEnter2D(Collider2D other) {
-        canvas.enabled = true;
-        correct_button.onClick.AddListener(Win);
+        if (isDead)
+            return;
+        
+        question_text.SetText(question);
+        question_text.enabled = true;
+
+        correct_text.SetText(correct_answer.ToString());
+        correct_button.SetActive(true);
+        incorrect_button.SetActive(true);        
+        correct_button.GetComponent<Button>().onClick.AddListener(Win);
 
         player.GetComponent<Movement>().enabled = false;
     }
 
     void Win() 
     {
+        isDead = true;
         player.GetComponent<Movement>().enabled = true;
         canvas.enabled = false;
         enemy_renderer.color = Color.red;
--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    Canvas canvas;
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
