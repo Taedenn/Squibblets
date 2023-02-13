870112a6f14f3076d9510b47ae747d51c9ec375c
Nathan Hutton <109482458+Nathan-Hutton@users.noreply.github.com>
2023-02-13 16:09:17 -0700

    Added a basic main menu and fixed another resolution scaling issue

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject button;
+
+    void Start() 
+    {
+        button.transform.GetComponent<Button>().onClick.AddListener(StartTutorial); 
+    }
+
+    void StartTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
