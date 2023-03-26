01303a538d49334f21cb38dfa1fbd556be362aeb
Taeden (they/them) <tca0103@westminstercollege.edu>
2023-03-26 01:02:54 -0600

    BossRoom script functional + furnishing wip

---
+++ Assets/Scripts/BossRoom.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossRoom : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    public PlayerController playerRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = player.GetComponent<PlayerController>();
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerRef.killCount == playerRef.desiredKillCount - 1){
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
